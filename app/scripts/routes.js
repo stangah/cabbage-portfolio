c28e938201069c9130bca03b6ba3e29e2c915b81
stangah <pstanger@gmail.com>
2014-10-28 11:29:35 -0700

    Some work on contact page

--- app/scripts/routes.js
+++ app/scripts/routes.js
@@ -50,6 +50,10 @@ var router = function($stateProvider, $urlRouterProvider) {
     .state('about', {
       url: '/about',
       templateUrl: '../views/about.html'
+    })
+    .state('contact', {
+      url: '/contact',
+      templateUrl: '../views/contact.html'
     });
 };
 
