b55f312f89dc115bb05cc05575a9fd473469318e
stangah <pstanger@gmail.com>
2014-12-01 19:29:57 -0800

    Commented out config file for now

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
-var jf = require('jsonfile');
-var nodemailer = require('nodemailer');
+// var jf = require('jsonfile');
+// var nodemailer = require('nodemailer');
 var app = express();
 
 app.use(express.static(__dirname + '/azure'));
@@ -9,16 +9,16 @@ app.get('/', function(req, res){
   res.sendfile(__dirname + '/azure/index.html');
 });
 
-var config = jf.readFileSync('etc/config.json');
+// var config = jf.readFileSync('etc/config.json');
 
-app.post('/contact', function(req, res) {
-  var transporter = nodemailer.createTransport({
-      service: 'Gmail',
-      auth: config.contactEmail
-  });
+// app.post('/contact', function(req, res) {
+//   var transporter = nodemailer.createTransport({
+//       service: 'Gmail',
+//       auth: config.contactEmail
+//   });
 
-  console.log(req);
-});
+//   console.log(req);
+// });
 
 var port = process.env.PORT || 9001;
 app.listen(port);
