7b3e08e3f1070946116d08878a5d7af3b88c2b9b
stangah <pstanger@gmail.com>
2015-05-04 11:58:17 -0700

    Fixed instagram icon

--- app/scripts/services/URLService.js
+++ app/scripts/services/URLService.js
@@ -4,8 +4,8 @@ var Links = {
   phone: 'tel:4157389592',
   email: 'mailto:chenjengjia@gmail.com',
   facebook: 'https://www.facebook.com/jiajia.chen.923',
+  instagram: 'https://instagram.com/jollyjiajia/',
   behance: 'https://www.behance.net/jiajiachen',
-  dribbble: 'https://dribbble.com/jiajiachen',
   linkedin: 'https://www.linkedin.com/profile/view?id=238830272'
 };
 
